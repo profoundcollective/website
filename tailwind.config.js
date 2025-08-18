cc9ab0718d2895d7894bd3af05874d2f0eff02e7
RJ McDermott <rj@profoundcollective.com>
2025-08-17 20:07:25 -0400

    added tailwind typograhpy for blog styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,22 +9,49 @@ module.exports = {
     ],
     theme: {
       fontFamily: {
-        'josefin': ['Josefin Sans', 'sans-serif']
+        'josefin': ['Josefin Sans', 'sans-serif'],
+        'rowdie': ['Rowdies'],
+        'roboto': ['Roboto'],
+        'zilla': ['Zilla Slab'],
+        'main': ['Verdana']
       },
       extend: {
         fontDisplay: {
-          'swap': 'swap',
+          'swap': 'swap'
+        },
+        typography: {
+          DEFAULT: {
+            css: {
+              color: '#E5E7EB', // gray-200
+              blockquote: {
+                borderLeftColor: '#10B981', // green-500
+                color: '#D1D5DB', // gray-300
+                fontStyle: 'normal',
+                fontSize: '1.5rem',
+                quotes: 'none',
+                marginTop: '2rem',
+                marginBottom: '2rem'
+              },
+              'blockquote p:first-of-type::before': {
+                content: 'none'
+              },
+              'blockquote p:last-of-type::after': {
+                content: 'none'
+              },
+              p: {
+                fontSize: '1.5rem',
+                lineHeight: '1.75'
+              },
+              li: {
+                fontSize: '1.5rem',
+                lineHeight: '1.75'
+              }
+            }
+          }
         }
-      },
-      extend: {
-        fontFamily: {
-          'rowdie': ['Rowdies'],
-          'josefin': ['Josefin Sans'],
-          'roboto': ['Roboto'],
-          'zilla': ['Zilla Slab'],
-          'main': ['Verdana'],
-        }
-      },
+      }
     },
-    plugins: []
+    plugins: [
+      require('@tailwindcss/typography')
+    ]
   }
